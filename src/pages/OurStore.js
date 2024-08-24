6f01b4afbd88a9f34aaa3d360c913bc0ea84aad7
shuvo-art <shuvo83qn@gmail.com>
2024-08-25 01:41:57 +0600

    Edit Our Store

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -128,9 +128,9 @@ const OurStore = () => {
                             <div className="d-flex justify-content-between align-items-center">
                                 <div className="d-flex align-items-center gap-10">
                                     <p className="mb-0 d-block full-width">Sort By:</p>
-                                    <select name="" id="" className="form-control form-select">
+                                    <select name="" defaultValue={"manual"} id="" className="form-control form-select">
                                         <option value="manual">Featured</option>
-                                        <option value="best-selling" selected="selected">Best Selling</option>
+                                        <option value="best-selling">Best Selling</option>
                                         <option value="title-ascending">Alphabetically, A-Z</option>
                                         <option value="title-descending">Alphabetically, Z-A</option>
                                         <option value="price-ascending">Price, low to high</option>
